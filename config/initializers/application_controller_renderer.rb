9f3155b1c101c9234020c13f46a1c936e5ddb566
higu1988 <ayaka.higuchi.0702@gmail.com>
2019-05-02 14:45:22 +0000

    addpost

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '61191f27dc0e7f1bddee4833b58dc1a382fa1aaebc5ba2b490d4c69caf2287a26384449a23b8798f35f12a2198eb2083fafaed1f61776d62393ce5cb9c6ee2d5'

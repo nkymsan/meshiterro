576af35230fc1f9881cf9bb2ec9c1ceea9c53dcd
higu1988 <ayaka.higuchi.0702@gmail.com>
2019-05-22 14:05:41 +0000

    add_fav

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root'post_images#index'
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
 end
